00de6b47de4a9a4024a4967ff9cf24dda153a5bf
shatayu <shatayu@outlook.com>
2017-03-26 07:36:17 -0700

    this better work

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
 var unirest = require("unirest");
 var prompt = require("prompt-sync")();
 var ConversationV1 = require("watson-developer-cloud/conversation/v1");
-var express    = require('express');
-var app        = express();    
-var bodyParser = require('body-parser');
-
 
 var conversation = new ConversationV1 ({
 	username: "d8543abd-4c79-43bb-9bc0-5f48a88a75cf",
@@ -12,6 +18,34 @@ var conversation = new ConversationV1 ({
 	path: { workspace_id: "4d835574-257e-4a41-b1f9-4b97669b495f" },
 	version_date: "2017-03-25"
 })
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
 // get code from front end
 function IDToRecipe(ID, callback) {
@@ -42,7 +76,6 @@ function getID (query, callback) {
 			.header("X-Mashape-Key", "7NFUuGWrn4msh6xdTxS0rv6FpdH6p14BsRyjsng0MeEmS0c6L2")
 			.header("Accept", "application/json")
 			.end(function (result) {
-			  	//console.log(result.status, result.headers, result.body);
 			  	return([result.status, result.headers, result.body]);
 			});
 }
@@ -65,25 +98,6 @@ function getIntent(input, callback) {
 		return intent;
 	})
 }
-
-//configure rest api
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // set our port
-
-var router = express.Router();              // get an instance of the express Router
-
-app.listen(port);
-console.log('Magic happens on port ' + port);
-
-router.get('/', function(req, res) {
-    res.json({ getIntent: 'hooray! welcome to our api!' });   
-});
-
-app.use('/api', router);
-
 
 router.use(function(req, res, next) {
     // do logging
@@ -111,11 +125,11 @@ router.route('/recipes/:recipe_string')
 
  router.route('/recipe/:recipe_id')
 	.get(function(req, res) {
-		getID(req.params.recipe_id, function(err, query) {
-			if(err) res.send(err);
-
-			res.json(query);
-		})
-        
+		unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?instructionsRequired=true&limitLicense=false&number=10&offset=0&query=" + req.params.recipe_id)
+			.header("X-Mashape-Key", "7NFUuGWrn4msh6xdTxS0rv6FpdH6p14BsRyjsng0MeEmS0c6L2")
+			.header("Accept", "application/json")
+			.end(function (result) {
+			  	res.json([result.status, result.headers, result.body]);
+			});       
         
     });
